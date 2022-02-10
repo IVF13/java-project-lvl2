bf5135c9357e2eb16c871ab94831db017eb18f5b
Ivan <iw.gordienko@yandex.ru>
2022-02-10 21:31:03 +0300

    Update DifferTest.java

--- src/test/java/hexlet.code/DifferTest.java
+++ src/test/java/hexlet.code/DifferTest.java
@@ -1,49 +1,43 @@
 package hexlet.code;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 public class DifferTest {
+    private File file1 = new File("src/test/resources/file1.json");
+    private File file2 = new File("src/test/resources/file2.json");
+    private String expected = "{\n"
+            +
+            "  - follow: false\n"
+            +
+            "    host: hexlet.io\n"
+            +
+            "  - proxy: 123.234.53.22\n"
+            +
+            "  - timeout: 50\n"
+            +
+            "  + timeout: 20\n"
+            +
+            "  + verbose: true\n"
+            +
+            "}";
 
     @Test
     void testDifferRelativePath() throws Exception {
-        File file1 = new File("src/test/resources/file1.json");
-        File file2 = new File("src/test/resources/file2.json");
-        String expected = "{\n"
-                +
-                "  - follow: false\n"
-                +
-                "    host: hexlet.io\n"
-                +
-                "  - proxy: 123.234.53.22\n"
-                +
-                "  - timeout: 50\n"
-                +
-                "  + timeout: 20\n"
-                +
-                "  + verbose: true\n"
-                +
-                "}";
         assertEquals(expected, Differ.generate(file1, file2));
     }
 
-    /*
+
     @Test
     void testDifferAbsolutePath() throws Exception {
-        File file1 = new File("E:/java-project-lvl2/src/test/resources/file1.json");
-        File file2 = new File("E:/java-project-lvl2/src/test/resources/file2.json");
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "    host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
-        assertEquals(expected, Differ.generate(file1, file2));
+        File file1WthAbsoltuePath = new File(file1.getAbsolutePath());
+        File file2WthAbsolutePath = new File(file2.getAbsolutePath());
+        assertEquals(expected, Differ.generate(file1WthAbsoltuePath, file2WthAbsolutePath));
     }
-*/
+
 
 }
 
