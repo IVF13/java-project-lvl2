10e8f1ed20fe92ff32a58c0d674b842d934d892f
Ivan <iw.gordienko@yandex.ru>
2022-02-13 00:01:54 +0300

    Update Differ.java

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,51 +1,18 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 
 public class Differ {
-    public static String generate(File file1, File file2) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data1 = mapper.readValue(file1, new TypeReference<>() { });
-        Map<String, Object> data2 = mapper.readValue(file2, new TypeReference<>() { });
-        StringBuilder result = new StringBuilder("{\n");
+    public static String generate(String format, File file1, File file2) throws Exception {
+        Map<String, Object> data1JSON = Parser.toParse(format, file1);
+        Map<String, Object> data2JSON = Parser.toParse(format, file2);
+        if (data1JSON != null && data2JSON != null) {
+            return Engine.toCompare(data1JSON, data2JSON);
+        }
+        return "format is incorrect\n supported formats:\n - JSON\n - YAML";
 
-        data2.forEach((key, value) -> {
-            if (!data1.containsKey(key)) {
-                data1.put(key + ":", value);
-            }
-        });
-
-        Map<String, Object> data1Sorted = data1.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .collect(Collectors
-                        .toMap(Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (e1, e2) -> e1,
-                                LinkedHashMap::new));
-
-        data1.clear();
-
-        data1Sorted.forEach((key, value) -> {
-            if (data2.containsKey(key) && data2.get(key).equals(value)) {
-                result.append("    " + key + ": " + value + "\n");
-            } else if (data2.containsKey(key) && !data2.get(key).equals(value)) {
-                result.append("  - " + key + ": " + value + "\n");
-                result.append("  + " + key + ": " + data2.get(key) + "\n");
-            } else if (key.endsWith(":")) {
-                result.append("  + " + key + " " + value + "\n");
-            } else {
-                result.append("  - " + key + ": " + value + "\n");
-            }
-        });
-        result.append("}");
-
-        return result.toString();
     }
 }
+
