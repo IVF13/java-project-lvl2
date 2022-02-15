593bca8684b81ebce2b158c96842218b8ce2e018
Ivan <iw.gordienko@yandex.ru>
2022-02-15 21:20:03 +0300

    Update Differ.java

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -6,13 +6,12 @@ import java.util.Map;
 
 public class Differ {
     public static String generate(String format, File file1, File file2) throws Exception {
-        Map<String, Object> data1JSON = Parser.toParse(format, file1);
-        Map<String, Object> data2JSON = Parser.toParse(format, file2);
+        Map<String, Object> data1JSON = Parser.toParse(file1);
+        Map<String, Object> data2JSON = Parser.toParse(file2);
         if (data1JSON != null && data2JSON != null) {
-            return Engine.toCompare(data1JSON, data2JSON);
+            return Engine.toCompare(format, data1JSON, data2JSON);
         }
-        return "format is incorrect\n supported formats:\n - JSON\n - YAML";
-
+        return "";
     }
 }
 
