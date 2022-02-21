c327477d56d27cda39bfd1f901a8107ba79bb183
Ivan <iw.gordienko@yandex.ru>
2022-02-21 21:00:02 +0300

    Update method return

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -3,14 +3,14 @@ package hexlet.code;
 import java.util.Map;
 
 public class Differ {
-    public static Object generate(String filePath1, String filePath2, String format) throws Exception {
+    public static String generate(String filePath1, String filePath2, String format) throws Exception {
         Map<String, Object> data1 = Parser.toParse(filePath1);
         Map<String, Object> data2 = Parser.toParse(filePath2);
 
         if (!isCollectionMapNull(data1) && !isCollectionMapNull(data2)) {
             Map<String, Object> sortedMergedMap = Builder.toBuildSortedMergedMap(data1, data2);
             String[][] resultArr = Builder.toBuildListOfDifferences(sortedMergedMap, data2);
-            return Formatter.toChooseFormat(format, resultArr);
+            return Formatter.toChooseFormat(format, resultArr).toString();
         }
 
         return null;
