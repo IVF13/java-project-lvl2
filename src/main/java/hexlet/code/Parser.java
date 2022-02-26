bf293648b2e8e1be985d78ecc85603c5e8e7dab7
Ivan <iw.gordienko@yandex.ru>
2022-02-26 05:08:09 +0300

    Update Parser.java

--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -3,24 +3,23 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> toParse(String filePath) throws IOException {
-        File file = new File(filePath);
-        if (file.toString().endsWith("json")) {
+    public static Map<String, Object> toParse(File data, String dataType) throws IOException {
+        if (dataType.equals(".json")) {
             ObjectMapper mapperJSON = new ObjectMapper();
-            return mapperJSON.readValue(file, new TypeReference<>() {
+            return mapperJSON.readValue(data, new TypeReference<>() {
             });
-        } else if (file.toString().endsWith("yml")) {
+        } else if (dataType.equals(".yml")) {
             ObjectMapper mapperYAML = new ObjectMapper(new YAMLFactory());
-            return mapperYAML.readValue(file, new TypeReference<>() {
+            return mapperYAML.readValue(data, new TypeReference<>() {
             });
         } else {
             return null;
         }
     }
+
 }
