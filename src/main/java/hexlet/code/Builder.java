988b71b566dc02df578954c77ab12cc193b75f8c
IVF13_notebook <iw.gordienko@yandex.ru>
2022-03-20 23:26:26 +0300

    Refactor methods

--- src/main/java/hexlet/code/Builder.java
+++ src/main/java/hexlet/code/Builder.java
@@ -8,14 +8,12 @@ import java.util.TreeSet;
 
 public class Builder {
     public static ArrayList<HashMap<String, Object>>
-    toBuildListOfDifferences(Map<String, Object> data1, Map<String, Object> data2) {
+        toBuildListOfDifferences(Map<String, Object> data1, Map<String, Object> data2) {
+        ArrayList<HashMap<String, Object>> internalRepresentationOfDifferences = new ArrayList<>();
         TreeSet<String> keys = new TreeSet<>();
 
         data2.forEach((key, value) -> keys.add(key));
-
         data1.forEach((key, value) -> keys.add(key));
-
-        ArrayList<HashMap<String, Object>> internalRepresentationOfDifferences = new ArrayList<>();
 
         for (String key : keys) {
             Object value1;
